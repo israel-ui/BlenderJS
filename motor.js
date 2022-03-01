b3bc5c41bc5fd4649c23fc034816fc798e27f49c
israel-ui <57158863+israel-ui@users.noreply.github.com>
2022-03-01 18:58:22 -0300

    blender complete

--- motor.js
+++ motor.js
@@ -1,12 +1,29 @@
 var blenderState = "off";
+var blender = document.getElementById("blender");
+var blenderbutton = document.getElementById("blender-button-sound")
+var blendersound = document.getElementById("blender-sound")
 
 function blendercontrol() {
     if (blenderState == "off") {
+        BrrBrr();
+        blender.classList.add("active");
         blenderState = "on";
-        console.log("you turned me on");
+/*         console.log("you turned me on");*/    
     } else {
         blenderState = "off"
-        console.log("you turned me off");
+        BrrBrr();
+        blender.classList.remove("active")
+/*         console.log("you turned me off");
+ */    }
+}
 
+function BrrBrr(){
+    if(blendersound.paused){
+        blenderbutton.play();
+        blendersound.play();
+    } else{
+        blenderbutton.play();
+        blendersound.pause();
+        blendersound.CurrentTime = 0;
     }
 }
